e76c9bfbe30919f45b1667b6ab63881f323165bc
Amirul Islam Hridoy <amirulislamhridoy30687@gmail.com>
2023-02-08 00:48:52 +0600

    add verify token for booking

--- api/booking.router.js
+++ api/booking.router.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const {addBooking} = require('../contollers/booking.contoler')
+const { verifyJWT } = require('../middware/verify')
 
-router.post('/add', addBooking)
+router.post('/add', verifyJWT, addBooking)
 
 module.exports = router
