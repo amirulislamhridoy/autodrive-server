fcb22be76155d559da7ff6a3ec03d9a6762f40a0
Amirul Islam Hridoy <amirulislamhridoy30687@gmail.com>
2023-02-08 16:27:38 +0600

    send user booking car data

--- api/booking.router.js
+++ api/booking.router.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const {addBooking, getAllBooking} = require('../contollers/booking.contoler')
+const {addBooking, getAllBooking, myCarBooking} = require('../contollers/booking.contoler')
 const { verifyJWT } = require('../middware/verify')
 
 router.get('/getAll', verifyJWT,  getAllBooking)
+router.get('/myCar', verifyJWT,  myCarBooking)
 router.post('/add', verifyJWT,  addBooking)
 
 
--- contollers/booking.contoler.js
+++ contollers/booking.contoler.js
@@ -22,6 +22,15 @@ const getAllBooking = async (req, res) => {
         
     }
 }
+const myCarBooking = async (req, res) => {
+    try{
+        const email = req.query.email
+        const bookings = await bookingModel.find({email})
+        res.status(200).send({data: bookings})
+    } catch(e){
+        res.status(204).json('Sorry, you have no booking car.')
+    }
+}
 const addBooking = async (req, res) => {
     try{
         const data = req.body
@@ -31,4 +40,4 @@ const addBooking = async (req, res) => {
         console.log(e.message)
     }
 }
-module.exports = {getAllBooking, addBooking}
+module.exports = {getAllBooking, myCarBooking, addBooking}
